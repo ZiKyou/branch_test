a5c5d09d017a8d5c2e68650dac39e0901b3b6eb0
ZiKyou <93782341+ZiKyou@users.noreply.github.com>
2022-01-18 11:53:16 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+first_num = int(input("Введите перове число: "))
+znak = input("Выберите действие + / -  : ")
+second_num = int(input("Введите второе число: "))
+
+if znak == "+":
+    print("Ваш ответ: ", first_num + second_num)
+
+elif znak == "-":
+    print("Ваш ответ: ", first_num - second_num)
+
+else:
+    print("error")
