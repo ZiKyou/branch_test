c849c90d025d8a11a2e6c79e070e330a4c8bcbd8
ZiKyou <93782341+ZiKyou@users.noreply.github.com>
2022-01-18 12:00:01 +0100

    Final version

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-
 first_num = int(input("Введите перове число: "))
-znak = input("Выберите действие + / -  : ")
+znak = input("Выберите действие + / - / * / /  : ")
 second_num = int(input("Введите второе число: "))
 
 if znak == "+":
@@ -8,6 +7,12 @@ if znak == "+":
 
 elif znak == "-":
     print("Ваш ответ: ", first_num - second_num)
+
+elif znak == "*":
+    print("Ваш ответ: ", first_num * second_num)
+
+elif znak == "/":
+    print("Ваш ответ: ", first_num / second_num)
 
 else:
     print("error")
